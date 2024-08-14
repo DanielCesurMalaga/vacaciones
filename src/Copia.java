07b522d3119f07c22ed1ede723853107698de3ac
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-08-14 16:17:53 +0200

    limpieza y solución con copia objetos

--- src/Copia.java
+++ src/Copia.java
@@ -1,26 +1,34 @@
 public class Copia {
    public static void main(String[] args) {
-      Numero uno = new Numero();
-      uno.num = 10;
-      Numero dos = uno;
-      dos.num = 20;
-      Numero.copia(uno, dos);
-      System.out.println("\n"+uno.hashCode()+"de Uno y su valor es: "+ uno.num);
-      System.out.println(dos.hashCode()+"de Dos y su valor es: "+ dos.num);
-      uno.num= 1000;
-      System.out.println("valor nuevo en Uno: "+uno.num);
-      System.out.println("valor nuevo en Dos: "+dos.num);
+      Numero num1 = new Numero(10);
+      Numero num2 = new Numero(20);
+      System.out.println();
+      System.out.println("num1: "+num1.hashCode()+", valor: "+num1.num);
+      System.out.println("num2: "+num2.hashCode()+", valor: "+num2.num);
+      num2 = num1;
 
+      System.out.println("num1: "+num1.hashCode()+", valor: "+num1.num);
+      System.out.println("num2: "+num2.hashCode()+", valor: "+num2.num);
+
+      num2.num = 50;
+      System.out.println("num1: "+num1.hashCode()+", valor: "+num1.num);
+      System.out.println("num2: "+num2.hashCode()+", valor: "+num2.num);
    }
 }
 
 class Numero {
    public int num;
 
+   public Numero(int num) {
+      this.num = num;
+   }
+
    public static void copia(Numero origin, Numero destino){
       destino.num = origin.num;
    }
+
    public static void clonacion(Numero origin, Numero destino){
       destino = origin;
    }
+
 }
